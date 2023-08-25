f7d5b1aee6495b8dbaaf270c24d85ca5472018fc
KINGCBERTON <stevekim724@gmail.com>
2023-08-25 13:32:46 +0300

    ui

--- lib/upload.dart
+++ lib/upload.dart
@@ -17,21 +17,9 @@ class PostDataScreen extends StatefulWidget {
 }
 
 class _PostDataScreenState extends State<PostDataScreen> {
-  File? _imageFile;
-  XFile? file;
+
 
   // Function to pick an image using the image_picker package
-  Future<void> _pickImage() async {
-    final picker = ImagePicker();
-
-    file = await picker.pickImage(source: ImageSource.gallery);
-
-    if (file != null) {
-      setState(() {
-        _imageFile = File(file!.path);
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +33,7 @@ class _PostDataScreenState extends State<PostDataScreen> {
               children: [
                 GestureDetector(
                   onTap: () {
-                    _pickImage();
+                
                   },
                   child: Container(
                     width: 200,
@@ -53,21 +41,14 @@ class _PostDataScreenState extends State<PostDataScreen> {
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey),
                     ),
-                    child: _imageFile != null
-                        ? kIsWeb
-                            ? Image.network(_imageFile!.path)
-                            : Image.file(_imageFile!)
-                        : const Center(
+                    child:const Center(
                             child: Text('Press to select image'),
                           ),
                   ),
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    Upload.instance.uploadimage(file);
-                    setState(() {
-                      file == null;
-                    });
+               
                   },
                   child: const Text('Submit'),
                 ),
