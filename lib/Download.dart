c849d5558318fb1d5dade904be5da48913ba1554
KINGCBERTON <stevekim724@gmail.com>
2023-08-28 14:27:13 +0300

    List

--- lib/Download.dart
+++ lib/Download.dart
@@ -1,13 +1,7 @@
-import 'dart:html';
-import 'dart:math';
 
-import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:gallery_saver/gallery_saver.dart';
 import 'package:storage/StorageController.dart';
-import 'package:path_provider/path_provider.dart';
 
 class DownloadDataScreen extends StatefulWidget {
   const DownloadDataScreen({super.key});
@@ -46,103 +40,9 @@ class _DownloadDataScreenState extends State<DownloadDataScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder(
-      future: imagefiles,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-              itemCount: snapshot.data!.items.length,
-              itemBuilder: (context, index) {
-                final imageRef = snapshot.data!.items[index];
-
-                return  Row(
-    children: [
-      FutureBuilder<String>(
-        future: imageRef.getDownloadURL(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return
-            
-             Row(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                      ),
-                      child: Image.network(
-                        snapshot.data!,
-                        width: 400,
-                        height: 200,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: IconButton(
-                        color: Colors.amber,
-                        onPressed: () async {
-                          final url = snapshot.data!;
-                          if (kIsWeb) {
-                            window.open(url, '_blank');
-                          } else {
-                            final tempfile = await getTemporaryDirectory();
-                            final path = '${tempfile.path}/${imageRef.name}';
-                            await Dio().download(url, path);
-                            await GallerySaver.saveImage(path, toDcim: true);
-                          }
-                        },
-                        icon: const Icon(Icons.download_outlined),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            );
-       
-          } else {
-            return const Text('Image not available.');
-          }
-        },
-      ),
-      const Spacer(),
-      Column(
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              try {
-                // Delete the file
-              await deleteAndRefreshList(imageRef);
-                print('File deleted successfully');
-              } catch (e) {
-                print('Error deleting file: $e');
-              }
-            },
-            child: Text('Delete File'),
-          ),
-          Text(imageRef.name),
-        ],
-      ),
-    ],
-  );
-              });
-        } else if (snapshot.hasError) {
-          return const Center(
-            child: Text('Unepected error found\n $e'),
-          );
-        } else {
-          return const Center(
+        body:  const Center(
             child: CircularProgressIndicator(),
-          );
-        }
-      },
+          
     ));
   }
 }
