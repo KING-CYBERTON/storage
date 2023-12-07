175ace8ca6145e19376e8a0a62a1143887fa7564
KINGCBERTON <stevekim724@gmail.com>
2023-12-07 19:20:25 +0300

    Api conseal

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyAmOB_-8DIEIwJEOvrM2a8C6RE-KbvSDDc",
-        projectId: "educative-9",
-        storageBucket: "educative-9.appspot.com",
-        messagingSenderId: "14205101883",
-        appId: "1:14205101883:web:49043452fcfe80de10791e",
+        apiKey: "{{API_Key}}",
+        projectId: "{{projectId}}",
+        storageBucket: "{{storageBucket}}",
+        databaseURL: "{{databaseURL}}",
+        messagingSenderId: "{{messagingSenderId}}",
+        appId: "{{appId}}",
       ),
     );
     runApp( MyApp());
